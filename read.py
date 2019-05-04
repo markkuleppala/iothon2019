f63df2955c45c9885999888c8cf019aa00082d19
leppalm1 <markku.leppala@aalto.fi>
2019-05-04 14:40:12 +0300

    Fixes on client and server

--- read.py
+++ read.py
@@ -20,7 +20,7 @@ def end_read(signal,frame):
 signal.signal(signal.SIGINT, end_read)
  
 # Create an object of the class MFRC522
-#MIFAREReader = MFRC522.MFRC522()
+MIFAREReader = mfrc522.MFRC522()
  
 # Welcome message
 print ("Welcome to the MFRC522 data read example")
