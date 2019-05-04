995f5aef189e09f5322fe05151855813661ddcda
leppalm1 <markku.leppala@aalto.fi>
2019-05-04 17:17:30 +0300

    Fixes on client and server

--- client.py
+++ client.py
@@ -10,9 +10,23 @@ IPADDRESS = "10.84.109.147" # Server IP address
 PORT = 6666 # Port used
 UID_accredited = [130,202,95,9,30] # Accredited RFID keycard
 UID_accredited = " ".join(str(x) for x in UID_accredited) # Modify the format into string
-#UID_accredited = str(UID_accredited)
 
 continue_reading = True
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print ("Ctrl+C captured, ending read.")
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+ 
+# Create an object of the class MFRC522
+MIFAREReader = mfrc522.MFRC522()
+
+print ("Press Ctrl-C to stop.")
 
 def tcp_connect(IPADDRESS, PORT):
     global s
@@ -52,5 +66,66 @@ def close_connection(): # Closing the pipe
 	s.close()
 
 initiate_connection(IPADDRESS, PORT)
-print(accreditation(UID_accredited))
+
+
+count = 0.0
+charging_active = False
+
+#Configure LED Output Pin
+LED = 7
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+ 
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+    
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+ 
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        #print ("Card detected")
+    
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+ 
+    # If we have the UID, continue
+
+    if status == MIFAREReader.MI_OK:
+ 
+        # Print UID
+        print ("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])+','+str(uid[4]))  
+        # This is the default key for authentication
+        #key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        
+        # Select the scanned tag
+        MIFAREReader.MFRC522_SelectTag(uid)
+        
+        #ENTER Your Card UID here
+        #my_uid = [130,202,95,9,30]
+
+        
+        
+        #Check to see if card UID read matches your card UID
+        if accreditation(uid):
+        #if uid == my_uid:                #Open the Doggy Door if matching UIDs
+            print("Access Granted")
+            charging_active = True
+            GPIO.output(LED, GPIO.HIGH)  #Turn on LED
+            charging_uid = uid
+            sleep(0.5)
+            #GPIO.output(LED, GPIO.LOW)   #Turn off LED
+        
+        else:                            #Don't open if UIDs don't match
+            print("Access Denied, YOU SHALL NOT PASS!")
+    else:
+        GPIO.output(LED, GPIO.LOW)
+        # if charging_active == True and count > 0:
+        #     print("UID: %s charged for %3.1f seconds" % (str(charging_uid),count))
+        #     count = 0
+        #     charging_active == False
+
+
+
+#print(accreditation(UID_accredited))
 close_connection()
